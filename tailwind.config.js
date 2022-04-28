ca3234ee5179e177a3010da255ad029ab5ea27c7
furquin <laert.ff@gmail.com>
2022-04-28 09:37:00 -0300

    feat: configura tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      width: {
+        450: '450px', 
+        250: '250px',
+        330: '330px'
+      },
+      height: {
+        450: '450px',
+        300: '300px',
+        330: '330px'
+      }
+    },
+  },
+  plugins: [],
+}
