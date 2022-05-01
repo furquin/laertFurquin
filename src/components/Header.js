0c6ffdca9cb85c4b8ef36ae8a8dd49506d1d3e76
furquin <laert.ff@gmail.com>
2022-05-01 18:32:32 -0300

    refactor: implementa links para contatos e tecnologias

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,21 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className="flex justify-around text-4xl bg-black text-white p-3">
-              
-        <Link to="/frontEnd">
-            <h3>Apps Front-end </h3>
-        </Link>
+      <Link to="/tecnologias">
+        <h3>Tecnologias</h3>
+      </Link>
+      
+      <Link to="/contatos">
+        <h3>Contatos</h3>
+      </Link>
+      
+      <Link to="/frontEnd">
+        <h3>Apps Front-end </h3>
+      </Link>
 
-        <Link to="/backEnd">
-            <h3>Apps Back-end </h3>      
-        </Link>
+      <Link to="/backEnd">
+        <h3>Apps Back-end </h3>      
+      </Link>
 
     </div>
   )
