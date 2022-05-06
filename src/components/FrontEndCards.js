708e155ccfbbdd12d159271787bd93cb24b783f0
furquin <laert.ff@gmail.com>
2022-05-06 12:33:32 -0300

    style:modfica css

--- src/components/FrontEndCards.js
+++ src/components/FrontEndCards.js
@@ -12,9 +12,9 @@ export function FrontEndCards(props) {
           </div>
           <div className='flex flex-col items-center' >
             <div className="flex justify-center opacity-85 hover:opacity-100">
-              <img className='rounded-3xl p-2 mt-2 w-300 h-300 hover:scale-105 transition-all duration-300 hover:border-4 border-orange-500' src={image} alt={name} />
+              <img className='rounded-3xl p-2 mt-2 w-250 h-250 hover:scale-105 transition-all duration-300 hover:border-4 border-orange-500' src={image} alt={name} />
             </div>
-            <div className='text-gray-500 ml-4 p-2 w-330 text-2xl'>
+            <div className='text-gray-500 ml-4 p-2 w-250 text-base'>
               <p>{ description }</p>
             </div>
           </div>  
