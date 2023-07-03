0c1a3e1efeeb35a0dbb733f8c07aba887910ef08
Laert Furquin <laert.ff@gmail.com>
2023-07-03 14:49:21 -0300

    Update Sobre.js

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -6,10 +6,6 @@ export class Sobre extends Component {
   render() {
     return (
       <div className="flex flex-col bg-gray-800 h-full w-full">
-        <div>
-          <h1 className="mt-10 flex text-white justify-center text-4xl " > Um pouco sobre mim  </h1>
-        </div>
-
         <div className='flex justify-around mt-16 mb-32 '>
 
           <div className="max-w-screen-sm" >
@@ -17,17 +13,19 @@ export class Sobre extends Component {
           </div>
           <div className= 'text-gray-500 max-w-xl text-2xl'>
             <p>
-              Prazer, me chamo Laert Furquin, tenho 25 anos.<br/>
+              Prazer, me chamo Laert Furquin, tenho 26 anos.<br/>
               <br/>
-              Sou Desenvolvedor Junior Full-Stack, em busca de uma oportunidade para aprimorar minhas habilidades em desenvolvimento web.<br/>
+              Sou Desenvolvedor Backend, buscando novas experiências para aprimorar minhas habilidades como desenvolvedor.<br/>
               <br/>
-              Tenho experiência com JavaScript, Typescript, NodeJS, ExpressJs, Sequelize, MySQL, MongoDB, ReactJS, Html e CSS.<br />
+              Tenho 2 anos de experiência com JavaScript, Typescript, NodeJS, TypeORM, Prisma, NestJS e Postgres. <br />
               <br/>
-              Gosto de buscar novos desafios visando sempre aprimorar meus conhecimentos e testar minha capacidade.<br />
+              <br>
+              Também tenho vasto conhecimento em ExpressJs, Sequelize, MySQL, MongoDB, ReactJS, Html e CSS, Vue, Quasar. <br />
               <br/>
-              Sou muito curioso e dedicado.<br />
+              Atualmente estou estudando análise de dados com python.<br />
+              <br/>
+              Sou muito organizado e dedicado.<br />
             </p>
-
           </div>
         </div>
       </div>
