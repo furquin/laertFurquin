370859b4321f3e9ea63cec144a5651e20ecb781c
Laert Furquin <laert.ff@gmail.com>
2023-07-03 14:23:00 -0300

    Update Created.js

--- src/components/Created.js
+++ src/components/Created.js
@@ -5,7 +5,7 @@ export function Created() {
     return (
      <div>
         <div className='flex justify-center bg-gray-700 p-2  w-full'>
-            <p className='text-lg text-white'>Created By Laert Furquin |  2022 All rights reserved.</p>
+            <p className='text-lg text-white'>Created By Laert Furquin |  2023 All rights reserved.</p>
         </div>
     </div>
     )  
